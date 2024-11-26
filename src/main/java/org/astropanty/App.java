9f60ae71b82e520c028f86e1b99f72d8a384fabd
yuuneeell <ecjoyosa@up.edu.ph>
2024-11-26 15:42:46 +0800

    scene height corretion

--- src/main/java/org/astropanty/App.java
+++ src/main/java/org/astropanty/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 960, 720); // change reso of scene to qHD
+        scene = new Scene(loadFXML("primary"), 960, 540); // change reso of scene to qHD
         stage.setScene(scene);
         stage.show();
     }
